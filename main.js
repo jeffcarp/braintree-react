e96ae0a4edcf677dd8f95c9411e2dc263421169a
Jeff Carpenter <gcarpenterv@gmail.com>
2015-02-04 02:50:47 +0000

    Futs with browserify

---
+++ main.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var braintree = require('braintree-web');
+
+var DropIn = React.createClass({
+  render: function() {
+    return React.DOM.div;
+  }
+});
+
+module.exports = {
+  dropin: DropIn
+};
